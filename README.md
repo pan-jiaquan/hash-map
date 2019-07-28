48f8d004ff792599701506f13c33f66625905352
jiaquan pan <pan.jiaquan@aliyun.com>
2019-07-28 20:12:50 +0800

    add primary code

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # hash-map
+
 Hash table based implementation of the Map interface
+
+## Installation
+
+via npm:
+``` bash
+$ npm install ihash-map
+```
+
+## Usage
+
+Full Map API is supported
+
+``` javascript
+import HashMap from 'ihash-map';
+
+interface ComparableObject {
+  hashCode(): string;
+  equals(other: ComparableObject): boolean;
+}
+
+class Point implements ComparableObject {
+  constructor(public x: number, public y: number) {}
+  hashCode() {
+    return `${this.x},${this.y}`;
+  }
+  equals(other: Point) {
+    return (
+      this === other || (other && this.x === other.x && this.y === other.y)
+    );
+  }
+}
+
+class Area implements ComparableObject {
+  constructor(public w: number, public h: number) {}
+  hashCode() {
+    return `${this.w},${this.h}`;
+  }
+  equals(other: Area) {
+    return (
+      this === other || (other && this.w === other.w && this.h === other.h)
+    );
+  }
+}
+
+const map = new HashMap<ComparableObject, unknown>(
+  one => one.hashCode(),
+  (one, other) => one.equals(other)
+);
+
+map.set(new Point(1, 2), 'P(1, 2)');
+map.get(new Point(1, 2));
+//=> 'P(1, 2)'
+map.get(new Area(1, 2));
+//=> undefined
+```
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,91 @@
+export default class HashMap<K, V> implements Map<K, V> {
+  private cache: { [code: string]: Array<[K, V]> } = Object.create(null);
+  private data = new Set<[K, V]>();
+  readonly [Symbol.toStringTag] = 'HashMap';
+
+  constructor(
+    public hashCode: (one: K) => string,
+    public equals: (one: K, other: K) => boolean
+  ) {}
+
+  clear() {
+    this.cache = Object.create(null);
+    this.data.clear();
+  }
+
+  delete(key: K) {
+    const code = this.hashCode(key);
+    const bucket = this.cache[code] || [];
+    const index = bucket.findIndex(([otherKey]) => this.equals(key, otherKey));
+    if (index !== -1) {
+      const [element] = bucket.splice(index, 1);
+      this.data.delete(element);
+      return true;
+    }
+    return false;
+  }
+
+  forEach(
+    callbackfn: (value: V, key: K, map: Map<K, V>) => void,
+    // tslint:disable-next-line:no-any
+    thisArg?: any
+  ) {
+    for (const [key, value] of this.entries()) {
+      callbackfn.apply(thisArg, [value, key, this]);
+    }
+  }
+
+  get(key: K) {
+    const code = this.hashCode(key);
+    const bucket = this.cache[code] || [];
+    const value = bucket.find(([otherKey]) => this.equals(key, otherKey));
+    return value && value[1];
+  }
+
+  has(key: K) {
+    const code = this.hashCode(key);
+    const bucket = this.cache[code] || [];
+    return bucket.some(([otherKey]) => this.equals(key, otherKey));
+  }
+
+  set(key: K, value: V) {
+    const code = this.hashCode(key);
+    const bucket = this.cache[code] || (this.cache[code] = []);
+    const index = bucket.findIndex(([otherKey]) => this.equals(key, otherKey));
+    const item: [K, V] = [key, value];
+    if (index !== -1) {
+      const [element] = bucket.splice(index, 1, item);
+      this.data.delete(element);
+    } else {
+      bucket.push(item);
+    }
+    this.data.add(item);
+    return this;
+  }
+
+  get size() {
+    return this.data.size;
+  }
+
+  [Symbol.iterator]() {
+    return this.data[Symbol.iterator]();
+  }
+
+  entries() {
+    return this.data.values();
+  }
+
+  keys() {
+    return this.createIterator(item => item[0]);
+  }
+
+  values() {
+    return this.createIterator(item => item[1]);
+  }
+
+  private *createIterator<T>(projection: (item: [K, V]) => T) {
+    for (const item of this.data) {
+      yield projection(item);
+    }
+  }
+}
